5bb947b035d61b5e7282225f0bc860245427a0d9
Al Zee <z@alz.ee>
2022-09-07 18:39:34 +0800

    swiper in index

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Swiper, SwiperItem } from '@tarojs/components'
 import './index.scss'
 import { AtButton, AtAvatar, AtTabBar, AtIcon } from 'taro-ui'
 
@@ -18,10 +18,31 @@ export default class Index extends Component<PropsWithChildren> {
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>按钮文案</AtButton>
-        <AtAvatar type='primary'></AtAvatar>
+
+      <Swiper
+      className='test-h'
+      indicatorColor='#999'
+      indicatorActiveColor='#333'
+      // vertical
+      circular
+      indicatorDots
+      autoplay>
+      <SwiperItem>
+      <View className='demo-text-1'>1</View>
+      </SwiperItem>
+      <SwiperItem>
+      <View className='demo-text-2'>2</View>
+      </SwiperItem>
+      <SwiperItem>
+      <View className='demo-text-3'>3</View>
+      </SwiperItem>
+      </Swiper>
+
+      <Text>Hello world!</Text>
+      <AtButton type='primary'>按钮文案</AtButton>
+      <AtAvatar type='primary'></AtAvatar>
+
       </View>
-    )
+      )
   }
 }
