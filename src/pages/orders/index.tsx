4667b55358e6e54049a4728a402e33e75c3a0108
Al Zee <z@alz.ee>
2022-09-08 01:54:50 +0800

    nth

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,6 +1,9 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtList, AtListItem, AtCard } from "taro-ui"
+import { AtNavBar } from 'taro-ui'
+import Taro from '@tarojs/taro'
 
 export default class Orders extends Component<PropsWithChildren> {
 
@@ -14,10 +17,68 @@ export default class Orders extends Component<PropsWithChildren> {
 
   componentDidHide () { }
 
+  handleClick () {
+    Taro.navigateBack()
+  }
+
   render () {
     return (
       <View className='orders'>
-        <Text>Hello world!</Text>
+        <AtNavBar
+          onClickRgIconSt={this.handleClick}
+          onClickRgIconNd={this.handleClick}
+          onClickLeftIcon={this.handleClick}
+          color='#000'
+          leftIconType='chevron-left'
+          fixed
+        />
+          <AtList className="list">
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          <AtListItem
+          title='代理商-请货'
+          note='2022-09-05 19:05:05'
+          extraText='50'
+          />
+          </AtList>
       </View>
     )
   }
