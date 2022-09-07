68a4f229d0f8efc689c9977bb403929f36e0e0fc
Al Zee <z@alz.ee>
2022-09-07 17:44:45 +0800

    tabbar

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,7 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
-import { AtButton, AtAvatar } from 'taro-ui'
+import { AtButton, AtAvatar, AtTabBar, AtIcon } from 'taro-ui'
 
 export default class Index extends Component<PropsWithChildren> {
 
@@ -21,6 +21,18 @@ export default class Index extends Component<PropsWithChildren> {
         <Text>Hello world!</Text>
         <AtButton type='primary'>按钮文案</AtButton>
         <AtAvatar type='primary'></AtAvatar>
+        <AtTabBar
+        fixed
+        tabList={[
+        { title: '首页', iconType: 'home' },
+        { title: '机构', iconType: 'link' },
+        // { title: '订单', iconType: 'list' },
+        // { title: '退货', iconType: 'list' },
+        // { title: '代金券', iconType: 'heart' },
+        // { title: '提现', iconType: 'money' },
+        { title: '我', iconType: 'user' }
+        ]}
+        />
       </View>
     )
   }
