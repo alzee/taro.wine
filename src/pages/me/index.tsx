94d6f192b1ac51e498a9f37459e151d4227c437c
Al Zee <z@alz.ee>
2022-09-07 18:39:24 +0800

    page me list

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,6 +1,7 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtList, AtListItem, AtCard } from "taro-ui"
 
 export default class Me extends Component<PropsWithChildren> {
 
@@ -17,8 +18,57 @@ export default class Me extends Component<PropsWithChildren> {
   render () {
     return (
       <View className='me'>
-        <Text>Hello world!</Text>
+
+      <AtList>
+      <AtListItem
+      title='代金券'
+      // note='描述信息'
+      // extraText='详细信息'
+      arrow='right'
+      thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+      />
+      <AtListItem
+      title='我的订单'
+      arrow='right'
+      thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+      />
+      <AtListItem
+      title='我的退货'
+      // note='描述信息'
+      arrow='right'
+      thumb='http://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png'
+      />
+      <AtListItem
+      title='我的提现'
+      // note='描述信息'
+      // extraText='详细信息'
+      arrow='right'
+      thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+      />
+      <AtListItem
+      title='门店销售'
+      // note='描述信息'
+      // extraText='详细信息'
+      arrow='right'
+      thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+      />
+      <AtListItem
+      title='餐厅消费'
+      // note='描述信息'
+      // extraText='详细信息'
+      arrow='right'
+      thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+      />
+      <AtListItem
+      title='我的信息'
+      // note='描述信息'
+      // extraText='详细信息'
+      arrow='right'
+      thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+      />
+      </AtList>
+
       </View>
-    )
+      )
   }
 }
