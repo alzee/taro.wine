b350b11fb450da2a899fa57c501a12c4b2b2850a
Al Zee <z@alz.ee>
2022-09-07 18:22:34 +0800

    no AtTabBar in pages

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -15,39 +15,12 @@ export default class Index extends Component<PropsWithChildren> {
 
   componentDidHide () { }
 
-  constructor () {
-    super(...arguments)
-    this.state = {
-      current: 0
-    }
-  }
-
-  handleClick (value) {
-    this.setState({
-      current: value
-    })
-  }
-
   render () {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
         <AtButton type='primary'>按钮文案</AtButton>
         <AtAvatar type='primary'></AtAvatar>
-        <AtTabBar
-        fixed
-        tabList={[
-        { title: '首页', iconType: 'home' },
-        { title: '机构', iconType: 'link' },
-        // { title: '订单', iconType: 'list' },
-        // { title: '退货', iconType: 'list' },
-        // { title: '代金券', iconType: 'heart' },
-        // { title: '提现', iconType: 'money' },
-        { title: '我', iconType: 'user' }
-        ]}
-        onClick={this.handleClick.bind(this)}
-        current={this.state.current}
-        />
       </View>
     )
   }
--- src/pages/org/index.tsx
+++ src/pages/org/index.tsx
@@ -15,34 +15,11 @@ export default class Org extends Component<PropsWithChildren> {
 
   componentDidHide () { }
 
-  constructor () {
-    super(...arguments)
-    this.state = {
-      current: 1
-    }
-  }
-
-  handleClick (value) {
-    this.setState({
-      current: value
-    })
-  }
-
   render () {
     return (
       <View className='org'>
         <Text>Hello world!</Text>
 
-        <AtTabBar
-        fixed
-        tabList={[
-        { title: '首页', iconType: 'home' },
-        { title: '机构', iconType: 'link' },
-        { title: '我', iconType: 'user' }
-        ]}
-        onClick={this.handleClick.bind(this)}
-        current={this.state.current}
-        />
       </View>
     )
   }
