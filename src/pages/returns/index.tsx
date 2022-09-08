08c7888cf625a8bf9a2ea69a6e22d4e83c6e4f42
Al Zee <z@alz.ee>
2022-09-08 09:45:59 +0800

    page returns

--- src/pages/returns/index.tsx
+++ src/pages/returns/index.tsx
@@ -1,6 +1,9 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtList, AtListItem, AtCard } from "taro-ui"
+import { AtNavBar } from 'taro-ui'
+import Taro from '@tarojs/taro'
 
 export default class Returns extends Component<PropsWithChildren> {
 
@@ -14,10 +17,68 @@ export default class Returns extends Component<PropsWithChildren> {
 
   componentDidHide () { }
 
+  handleClick () {
+    Taro.navigateBack()
+  }
+
   render () {
     return (
       <View className='returns'>
-        <Text>Hello world!</Text>
+        <AtNavBar
+          onClickRgIconSt={this.handleClick}
+          onClickRgIconNd={this.handleClick}
+          onClickLeftIcon={this.handleClick}
+          color='#000'
+          leftIconType='chevron-left'
+          fixed
+        />
+          <AtList className="list first">
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          <AtListItem
+          title='五粮液500ml'
+          note='2022-09-05 19:05:05'
+          extraText='已完成'
+          />
+          </AtList>
       </View>
     )
   }
