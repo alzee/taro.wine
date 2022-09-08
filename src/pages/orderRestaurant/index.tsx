12ff0889e91d6f0ee182edcb4cf1c895dc058a9e
Al Zee <z@alz.ee>
2022-09-08 09:58:21 +0800

    page orderRestaurant

--- src/pages/orderRestaurant/index.tsx
+++ src/pages/orderRestaurant/index.tsx
@@ -1,6 +1,9 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtList, AtListItem, AtCard } from "taro-ui"
+import { AtNavBar } from 'taro-ui'
+import Taro from '@tarojs/taro'
 
 export default class Orderrestaurant extends Component<PropsWithChildren> {
 
@@ -14,10 +17,68 @@ export default class Orderrestaurant extends Component<PropsWithChildren> {
 
   componentDidHide () { }
 
+  handleClick () {
+    Taro.navigateBack()
+  }
+
   render () {
     return (
       <View className='orderRestaurant'>
-        <Text>Hello world!</Text>
+        <AtNavBar
+          onClickRgIconSt={this.handleClick}
+          onClickRgIconNd={this.handleClick}
+          onClickLeftIcon={this.handleClick}
+          color='#000'
+          leftIconType='chevron-left'
+          fixed
+        />
+          <AtList className="list first">
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          <AtListItem
+          title='餐厅订单号 12345'
+          note='2022-09-05 19:05:05'
+          extraText='代金券 50'
+          />
+          </AtList>
       </View>
     )
   }
