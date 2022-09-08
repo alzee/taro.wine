f97a7378028bfa092a16e8818baf465ec1241a93
Al Zee <z@alz.ee>
2022-09-08 10:08:25 +0800

    page myInfo

--- src/pages/myInfo/index.tsx
+++ src/pages/myInfo/index.tsx
@@ -1,6 +1,10 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtList, AtListItem, AtCard } from "taro-ui"
+import { AtNavBar } from 'taro-ui'
+import { AtButton } from 'taro-ui'
+import Taro from '@tarojs/taro'
 
 export default class Myinfo extends Component<PropsWithChildren> {
 
@@ -14,10 +18,38 @@ export default class Myinfo extends Component<PropsWithChildren> {
 
   componentDidHide () { }
 
+  handleClick () {
+    Taro.navigateBack()
+  }
+
   render () {
     return (
       <View className='myInfo'>
-        <Text>Hello world!</Text>
+        <AtNavBar
+          onClickRgIconSt={this.handleClick}
+          onClickRgIconNd={this.handleClick}
+          onClickLeftIcon={this.handleClick}
+          color='#000'
+          leftIconType='chevron-left'
+          fixed
+        />
+          <AtList className="list first">
+          <AtListItem
+          title='昵称'
+          extraText='Tom'
+          arrow='right'
+          />
+          <AtListItem
+          title='头像'
+          arrow='right'
+          />
+          <AtListItem
+          title='ID'
+          extraText='55'
+          arrow='right'
+          />
+          </AtList>
+		<AtButton className='btn' type='secondary' size='small'>退出登录</AtButton>
       </View>
     )
   }
