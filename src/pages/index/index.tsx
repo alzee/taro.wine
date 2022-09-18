93730bfb28d9f2ab6812826224e9f59cc74246f0
Al Zee <z@alz.ee>
2022-09-18 19:12:54 +0800

    nth

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -8,15 +8,17 @@ import Taro from '@tarojs/taro'
 import img1 from './img/400x220.png'
 import img2 from './img/80x80.png'
 
-interface Data {
-  [propName: string]: any;
-}
-
 export default class Index extends Component<PropsWithChildren> {
   baseUrl = 'https://127.0.0.1:8000/';
   apiUrl = this.baseUrl + 'api/nodes/';
   imgUrl = this.baseUrl + 'uploads/';
   nodes = [];
+  nodes0 = [];
+  nodes1 = [];
+  nodes2 = [];
+  list0 = [];
+  list1 = [];
+  list2 = [];
 
   componentWillMount () { }
 
@@ -24,14 +26,53 @@ export default class Index extends Component<PropsWithChildren> {
     const self = this;
     Taro.request({
         url: this.apiUrl,
-        data: {
-          x: '',
-          y: ''
-        },
-        that: this,
         success: function (res) { self.setState({data: res.data}) }
         }).then((res) =>{
           this.nodes = res.data;
+          for (let i in this.nodes) {
+            if (this.nodes[i].tag == 0) {
+            this.nodes0.push(this.nodes[i]);
+            }
+            // if (this.nodes[i].tag == 1) {
+            // this.nodes1.push(this.nodes[i]);
+            // }
+            if (this.nodes[i].tag == 2) {
+            this.nodes2.push(this.nodes[i]);
+            }
+          }
+          for (let i in this.nodes0) {
+            if (i < 3) {
+            this.list0.push(
+                <SwiperItem>
+                <View className=''>
+                <Image src={ this.imgUrl + this.nodes0[i].img }></Image>
+                </View>
+                </SwiperItem>
+                );
+            }
+          }
+          // for (let i in nodes1) {
+          //   if (i < 4) {
+          //     list1.push(
+          //         <View className="at-col">
+          //             <AtAvatar className="avatar" circle size="small" image={ this.state && this.state.data && this.imgUrl+ nodes1[i].img}></AtAvatar>
+          //             <Text>{nodes1[i].title}</Text>
+          //         </View>
+          //         );
+          //   }
+          // }
+          for (let i in this.nodes2) {
+            if (i < 2) {
+              this.list2.push(
+                  <View className="highlight2">
+                  <Image className='img' src={this.imgUrl + this.nodes2[i].img}></Image>
+                  <Text className="text">
+                  {this.nodes2[i].title}
+                  </Text>
+                  </View>
+                  );
+            }
+          }
           })
   }
 
@@ -42,57 +83,13 @@ export default class Index extends Component<PropsWithChildren> {
   componentDidHide () { }
 
   render () {
-    let nodes0 = [];
-    let nodes1 = [];
-    let nodes2 = [];
-    let list0 = [];
-    let list1 = [];
-    let list2 = [];
-    for (let i in this.nodes) {
-      if (this.nodes[i].tag == 0) {
-        nodes0.push(this.nodes[i]);
-      }
-      if (this.nodes[i].tag == 1) {
-        nodes1.push(this.nodes[i]);
-      }
-      if (this.nodes[i].tag == 2) {
-        nodes2.push(this.nodes[i]);
-      }
-    }
-    for (let i in nodes0) {
-      if (i < 3) {
-        list0.push(
-            <SwiperItem>
-            <View className='demo-text-1'><Image src={ this.imgUrl + nodes0[i].img }></Image></View>
-            </SwiperItem>
-            );
-      }
-    }
-    // for (let i in nodes1) {
-    //   if (i < 4) {
-    //     list1.push(
-    //         <View className="at-col">
-    //             <AtAvatar className="avatar" circle size="small" image={ this.state && this.state.data && this.imgUrl+ nodes1[i].img}></AtAvatar>
-    //             <Text>{nodes1[i].title}</Text>
-    //         </View>
-    //         );
-    //   }
-    // }
-    for (let i in nodes2) {
-      if (i < 2) {
-        list2.push(
-        <View className="highlight2">
-            <Image className='img' src={this.imgUrl + nodes2[i].img}>11</Image>
-        </View>
-            );
-      }
-    }
     return (
       <View className='index'>
         <AtSearchBar
         actionName='搜一下'
         />
 
+        { this.state && this.state.data &&
         <Swiper
         className='swiper'
         indicatorColor='#999'
@@ -101,8 +98,9 @@ export default class Index extends Component<PropsWithChildren> {
         circular
         indicatorDots
         autoplay>
-        {list0}
+        {this.list0}
         </Swiper>
+        }
 
         <View className='at-row highlight1'>
           <View className="at-col">
@@ -123,7 +121,7 @@ export default class Index extends Component<PropsWithChildren> {
           </View>
         </View>
 
-        {list2}
+        {this.state && this.state.data && this.list2}
       </View>
       )
   }
