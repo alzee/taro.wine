b4f64fc5e79c28a5d6951df2f12040c19a264b06
Al Zee <z@alz.ee>
2022-09-19 12:45:33 +0800

    nth

--- src/pages/node/index.tsx
+++ src/pages/node/index.tsx
@@ -1,5 +1,5 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components'
 import './index.scss'
 
 export default class Node extends Component<PropsWithChildren> {
@@ -16,8 +16,29 @@ export default class Node extends Component<PropsWithChildren> {
 
   render () {
     return (
-      <View className='node'>
-        <Text>Hello world!</Text>
+      <View className='at-article'>
+      <View className='at-article__h1'>
+      这是一级标题这是一级标题
+      </View>
+      <View className='at-article__info'>
+      2017-05-07&nbsp;&nbsp;&nbsp;这是作者
+      </View>
+      <View className='at-article__content'>
+      <View className='at-article__section'>
+      <View className='at-article__h2'>这是二级标题</View>
+      <View className='at-article__h3'>这是三级标题</View>
+      <View className='at-article__p'>
+      这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。1234567890123456789012345678901234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ
+      </View>
+      <View className='at-article__p'>
+      这是文本段落。这是文本段落。
+      </View>
+      <Image 
+      className='at-article__img' 
+      src='https://jdc.jd.com/img/400x400' 
+      mode='widthFix' />
+      </View>
+      </View>
       </View>
     )
   }
