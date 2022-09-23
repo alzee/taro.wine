d24ad748f74297a87c37c7b9d6d51916ee30d3ba
Al Zee <z@alz.ee>
2022-09-23 12:26:09 +0800

    nth

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,4 +1,5 @@
-const baseUrl = 'https://127.0.0.1:8000/';
+// const baseUrl = 'https://127.0.0.1:8000/';
+const baseUrl = 'http://172.23.203.98:8000/';
 
 export const Env = {
   apiUrl: baseUrl + 'api/',
