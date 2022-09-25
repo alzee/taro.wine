4c24a0b949e946878a8dfe9f69c5b05941167b39
Al Zee <z@alz.ee>
2022-09-25 11:03:45 +0800

    nth

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,12 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import Taro from '@tarojs/taro'
+import type CustomTabBar from '../../custom-tab-bar'
+import { Env } from '../../env/env'
 
 export default class Product extends Component<PropsWithChildren> {
+  pageCtx = Taro.getCurrentInstance().page
 
   componentWillMount () { }
 
@@ -10,7 +14,10 @@ export default class Product extends Component<PropsWithChildren> {
 
   componentWillUnmount () { }
 
-  componentDidShow () { }
+  componentDidShow () {
+    const tabbar = Taro.getTabBar<CustomTabBar>(this.pageCtx)
+    tabbar?.setSelected(0)
+  }
 
   componentDidHide () { }
 
