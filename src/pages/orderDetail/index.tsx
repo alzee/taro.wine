706fc07c07179dac97cf28b4767af5367073043f
Al Zee <z@alz.ee>
2022-09-27 17:21:24 +0800

    nth

--- src/pages/orderDetail/index.tsx
+++ src/pages/orderDetail/index.tsx
@@ -1,12 +1,25 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { Env } from '../../env/env'
+import Taro from '@tarojs/taro'
 
 export default class Orderdetail extends Component<PropsWithChildren> {
+  instance = Taro.getCurrentInstance();
+  id: int
 
   componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount () {
+    this.id = this.instance.router.params.id
+    const self = this;
+    Taro.request({
+      url: Env.apiUrl + 'orders/' + this.id,
+      success: function (res) { self.setState({data: res.data}) }
+    }).then((res) =>{
+      this.node = res.data
+    })
+  }
 
   componentWillUnmount () { }
 
@@ -17,7 +30,7 @@ export default class Orderdetail extends Component<PropsWithChildren> {
   render () {
     return (
       <View className='orderDetail'>
-        <Text>Hello world!</Text>
+        <Text>Hello world! id is {this.id}</Text>
       </View>
     )
   }
