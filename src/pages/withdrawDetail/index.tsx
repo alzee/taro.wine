ba5a17a534617d9cdfa3d2e6affca4c1571bc737
Al Zee <z@alz.ee>
2022-09-28 10:19:35 +0800

    nth

--- src/pages/withdrawDetail/index.tsx
+++ src/pages/withdrawDetail/index.tsx
@@ -22,6 +22,8 @@ export default class Withdrawdetail extends Component<PropsWithChildren> {
     }).then((res) =>{
       this.entity = res.data
       console.log(this.entity)
+      console.log(this.entity.applicant)
+      console.log(this.entity.approver)
     })
   }
 
@@ -34,13 +36,17 @@ export default class Withdrawdetail extends Component<PropsWithChildren> {
   render () {
     return (
       <View className='withdrawDetail'>
+      { this.state &&
       <AtList>
-      <AtListItem title='状态' extraText={Taxon.status[this.entity.status]} />
+      <AtListItem title='申请方' extraText={this.entity.applicant.name} />
+      <AtListItem title='审核方' extraText={this.entity.approver.name} />
       <AtListItem title='申请金额' extraText={this.entity.amount / 100} />
       <AtListItem title='实际到账' extraText={this.entity.actualAmount / 100} />
       <AtListItem title='日期' extraText={this.entity.date} />
+      <AtListItem title='状态' extraText={Taxon.status[this.entity.status]} />
       <AtListItem title='备注' extraText={this.entity.note} />
       </AtList>
+      }
       </View>
     )
   }
