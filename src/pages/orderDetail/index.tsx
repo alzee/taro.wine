f6621af8ac5115db518ea568edb54c56dda64525
Al Zee <z@alz.ee>
2022-09-28 11:03:57 +0800

    nth

--- src/pages/orderDetail/index.tsx
+++ src/pages/orderDetail/index.tsx
@@ -3,10 +3,13 @@ import { View, Text } from '@tarojs/components'
 import './index.scss'
 import { Env } from '../../env/env'
 import Taro from '@tarojs/taro'
+import { AtButton, AtList, AtListItem} from "taro-ui"
+import { Taxon } from '../../Taxon'
 
 export default class Orderdetail extends Component<PropsWithChildren> {
   instance = Taro.getCurrentInstance();
   id: int
+  entity = {}
 
   componentWillMount () { }
 
@@ -17,7 +20,8 @@ export default class Orderdetail extends Component<PropsWithChildren> {
       url: Env.apiUrl + 'orders/' + this.id,
       success: function (res) { self.setState({data: res.data}) }
     }).then((res) =>{
-      this.node = res.data
+      this.entity = res.data
+      console.log(this.entity)
     })
   }
 
@@ -30,7 +34,26 @@ export default class Orderdetail extends Component<PropsWithChildren> {
   render () {
     return (
       <View className='orderDetail'>
-        <Text>Hello world! id is {this.id}</Text>
+      { this.state &&
+      <AtList>
+      <AtListItem title='发货方' extraText={this.entity.seller.name} />
+      <AtListItem title='进货方' extraText={this.entity.buyer.name} />
+      <AtListItem title='订单商品' />
+      <View className='store'>
+        <Text>Hello world!</Text>
+      </View>
+      <View className='store'>
+        <Text>Hello world!</Text>
+      </View>
+      <View className='store'>
+        <Text>Hello world!</Text>
+      </View>
+      <AtListItem title='金额' extraText={this.entity.amount / 100} />
+      <AtListItem title='代金券' extraText={this.entity.voucher / 100} />
+      <AtListItem title='日期' extraText={this.entity.date} />
+      <AtListItem title='备注' extraText={this.entity.note} />
+      </AtList>
+      }
       </View>
     )
   }
