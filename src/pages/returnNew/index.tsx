5f79fe1a636fcda598aa5f9e2749ea0f413b28ea
Al Zee <z@alz.ee>
2022-09-29 17:20:08 +0800

    nth

--- src/pages/returnNew/index.tsx
+++ src/pages/returnNew/index.tsx
@@ -84,7 +84,7 @@ export default class Returnnew extends Component<PropsWithChildren> {
         success: () => {
           setTimeout(
             () => {
-              Taro.redirectTo({url: '/pages/orders/index'})
+              Taro.switchTab({url: '/pages/orders/index'})
             }, 500
           )
         }
