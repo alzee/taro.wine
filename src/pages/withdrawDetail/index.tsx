549b3e1b1cb7cce761df10159d10991d8385a716
Al Zee <z@alz.ee>
2022-09-30 15:19:20 +0800

    nth

--- src/pages/withdrawDetail/index.tsx
+++ src/pages/withdrawDetail/index.tsx
@@ -1,5 +1,5 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import './index.scss'
 import { Env } from '../../env/env'
 import Taro from '@tarojs/taro'
@@ -10,6 +10,7 @@ import { fmtDate } from '../../fmtDate'
 export default class Withdrawdetail extends Component<PropsWithChildren> {
   instance = Taro.getCurrentInstance();
   id: int
+  oid: int
   entity = {}
 
   componentWillMount () { }
@@ -17,6 +18,17 @@ export default class Withdrawdetail extends Component<PropsWithChildren> {
   componentDidMount () {
     this.id = this.instance.router.params.id
     const self = this;
+
+    Taro.getStorage({
+      key: Env.storageKey,
+      success: res => {
+        this.oid = res.data.org.id
+      },
+      fail: res => {
+        console.log('fuck')
+      },
+    });
+
     Taro.request({
       url: Env.apiUrl + 'withdraws/' + this.id,
       success: function (res) { self.setState({data: res.data}) }
@@ -47,6 +59,17 @@ export default class Withdrawdetail extends Component<PropsWithChildren> {
       <AtListItem title='状态' extraText={Taxon.status[this.entity.status]} />
       <AtListItem title='备注' extraText={this.entity.note} />
       </AtList>
+      }
+      { this.entity.status > 0 &&
+      <View className='btn-wrapper'>
+        <Button className='btn' type='default' disabled>{Taxon.status[this.entity.status]}</Button>
+      </View>
+      }
+      { this.entity.status == 0 &&  this.entity.approver.id == this.oid &&
+      <View className='btn-wrapper'>
+      <Button className='btn' type='primary' size=''>通过</Button>
+      <Button className='btn' type='warn' size=''>拒绝</Button>
+      </View>
       }
       </View>
     )
