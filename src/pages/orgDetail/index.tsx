28352c16033077b9786190e9278cf6f7e1dcabac
Al Zee <z@alz.ee>
2022-10-06 14:43:42 +0800

    data in state in page orgDetail

--- src/pages/orgDetail/index.tsx
+++ src/pages/orgDetail/index.tsx
@@ -9,7 +9,7 @@ import { Taxon } from '../../Taxon'
 export default class Orgdetail extends Component<PropsWithChildren> {
   instance = Taro.getCurrentInstance();
   id: int
-  entity = {}
+  state = {}
 
   componentWillMount () { }
 
@@ -18,10 +18,8 @@ export default class Orgdetail extends Component<PropsWithChildren> {
     const self = this;
     Taro.request({
       url: Env.apiUrl + 'orgs/' + this.id,
-      success: function (res) { self.setState({data: res.data}) }
+      success: function (res) { self.setState({entity: res.data}) }
     }).then((res) =>{
-      this.entity = res.data
-      console.log(this.entity)
     })
   }
 
@@ -34,14 +32,14 @@ export default class Orgdetail extends Component<PropsWithChildren> {
   render () {
     return (
       <View className='orgDetail'>
-      <Image mode='widthFix' className='storefront' src={this.entity.img && Env.imgUrl + 'org/' + this.entity.img} />
+      <Image mode='widthFix' className='storefront' src={this.state.entity.img && Env.imgUrl + 'org/' + this.state.entity.img} />
       <AtList>
-      <AtListItem title='名称' extraText={this.entity.name} />
-      <AtListItem title='类型' extraText={Taxon.orgType[this.entity.type]} />
-      <AtListItem title='联系人' extraText={this.entity.contact} />
-      <AtListItem title='电话' extraText={this.entity.phone} />
-      <AtListItem title='地址' extraText={this.entity.address} />
-      <AtListItem title='地区' extraText={this.entity.district} />
+      <AtListItem title='名称' extraText={this.state.entity.name} />
+      <AtListItem title='类型' extraText={Taxon.orgType[this.state.entity.type]} />
+      <AtListItem title='联系人' extraText={this.state.entity.contact} />
+      <AtListItem title='电话' extraText={this.state.entity.phone} />
+      <AtListItem title='地址' extraText={this.state.entity.address} />
+      <AtListItem title='地区' extraText={this.state.entity.district} />
       </AtList>
       </View>
     )
