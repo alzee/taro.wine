5fcc8a12694f3ab82994c14d0ecb1676498bb82f
Al Zee <z@alz.ee>
2022-12-02 20:06:02 +0800

    test backend url

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,7 @@
 // const baseUrl = 'https://127.0.0.1:8000/';
 // const baseUrl = 'http://localhost:8000/';
-const baseUrl = 'https://hbljk.cn/';
+const baseUrl = 'https://jiu.itove.com/';
+// const baseUrl = 'https://hbljk.cn/';
 
 export const Env = {
   baseUrl: baseUrl,
