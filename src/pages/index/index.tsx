6d8ff547e918fc07b1c8ddd6de7bae708f203463
Al Zee <z@alz.ee>
2023-01-16 23:55:29 +0800

    nth

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -69,9 +69,9 @@ export default class Index extends Component<PropsWithChildren> {
         featured.push(
           <View className="item at-col at-col-6" key={i}>
           <Image className='img' mode='widthFix' src={this.imgUrl + 'node/' + i.img} onClick={()=>this.navToNode(i.id)} ></Image>
-          <Text className="text">
+          <View className="text">
           {i.title}
-          </Text>
+          </View>
           </View>
         );
       }
