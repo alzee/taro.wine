4642660972f77e2593eb7f507140d50f857a9209
Al Zee <z@alz.ee>
2023-01-17 03:57:06 +0800

    page productDetail refactor

--- src/pages/productDetail/index.tsx
+++ src/pages/productDetail/index.tsx
@@ -1,10 +1,12 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text, Image } from '@tarojs/components'
+import { View, Text, Image, Button } from '@tarojs/components'
 import './index.scss'
 import { Env } from '../../env/env'
 import { Taxon } from '../../Taxon'
 import Taro from '@tarojs/taro'
-import { AtButton, AtList, AtListItem} from "taro-ui"
+import { AtButton, AtList, AtListItem, AtIcon } from "taro-ui"
+import checkIcon from '../../icon/check-circle-fill.svg'
+import shopIcon from '../../icon/shop.svg'
 
 export default class Productdetail extends Component<PropsWithChildren> {
   instance = Taro.getCurrentInstance();
@@ -30,11 +32,87 @@ export default class Productdetail extends Component<PropsWithChildren> {
   componentDidHide () { }
 
   render () {
+    console.log(checkIcon)
     return (
       <View className='productDetail'>
       { this.state.entity &&
-      <AtList>
+        <>
       <Image className='pic rounded' mode='aspectFill' src={Env.imgUrl + 'product/' + this.state.entity.img} />
+      <View className='price'>
+        <View className='amount'>
+        {this.state.entity.price / 100} 元
+        </View>
+        <View className='note'>
+          <Text className='text'>
+          已售22件
+          </Text>
+        <AtIcon value='heart-2' size='15' color='#ff3333'></AtIcon>
+        </View>
+      </View>
+
+      <View className='title'>
+        {this.state.entity.name}
+      </View>
+      
+      <View className='spec'>
+        <View className='item'>
+          <Text className='label'> 编号 </Text>
+          {this.state.entity.sn}
+        </View>
+        <View className='item'>
+          <Text className='label'> 规格 </Text>
+          {this.state.entity.spec}
+        </View>
+        <View className='item'>
+          <Text className='label'> 随赠代金券 </Text>
+          <Text className='voucher'>
+          {this.state.entity.voucher / 100} 元
+          </Text>
+        </View>
+      </View>
+
+      <View className='attr'>
+        <View className='item'>
+        <Image className='img' src={checkIcon} />
+        免邮费
+        </View>
+        <View className='item'>
+        <Image className='img' src={checkIcon} />
+        正品保障
+        </View>
+        <View className='item'>
+        <Image className='img' src={checkIcon} />
+        7天无理由退货
+        </View>
+      </View>
+
+      <View className='review'>
+        <View className='section'> 商品评价 (88)</View>
+        <View className='content'> 云想衣裳花想容，春风拂槛露华浓。</View>
+        <View className='note'> 用户user1 发布于12月30日</View>
+        <View className='star'>
+          <AtIcon value='star-2' size='15' color='#ff3333'></AtIcon>
+          <AtIcon value='star-2' size='15' color='#ff3333'></AtIcon>
+          <AtIcon value='star-2' size='15' color='#ff3333'></AtIcon>
+          <AtIcon value='star-2' size='15' color='#ff3333'></AtIcon>
+          <AtIcon value='star-2' size='15' color='#ff3333'></AtIcon>
+          <Text> 5.0分</Text>
+        </View>
+      </View>
+
+      <View className='bottom'>
+        <View className='left'>
+          <View className=''>
+            <Image className='img' src={shopIcon} />
+          </View>
+          <View className='text'> 店铺 </View>
+        </View>
+        <Button className='right' size='mini'>立即购买</Button>
+      </View>
+        </>
+      
+      /*
+      <AtList>
       <AtListItem title='产品名称' extraText={this.state.entity.name} />
       <AtListItem title='产品编号' extraText={this.state.entity.sn} />
       <AtListItem title='产品规格' extraText={this.state.entity.spec} />
@@ -42,6 +120,7 @@ export default class Productdetail extends Component<PropsWithChildren> {
       <AtListItem title='产品库存' extraText={this.state.entity.stock} />
       <AtListItem title='随赠代金券' extraText={this.state.entity.voucher / 100} />
       </AtList>
+     */
       }
       </View>
     )
