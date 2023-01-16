93e48cb097c070a84fbe36aaf56e217047e66d3f
Al Zee <z@alz.ee>
2023-01-16 21:06:56 +0800

    fix AtInput issue

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,12 @@
 module.exports = {
+  // https://github.com/NervJS/taro-ui/issues/1555#issuecomment-1334761262
+  framework: 'react',
+  compiler: {
+    type: 'webpack5',
+    prebundle: {
+      exclude: ['taro-ui']
+    }
+  },
   env: {
     NODE_ENV: '"development"'
   },
