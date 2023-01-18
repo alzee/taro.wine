766aa8413d0375541b556e977582aecf0f84c7e7
Al Zee <z@alz.ee>
2023-01-19 06:14:32 +0800

    nth

--- src/pages/orgReferral/index.tsx
+++ src/pages/orgReferral/index.tsx
@@ -1,23 +1,103 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import './index.scss'
+import { AtList, AtListItem, AtCard } from "taro-ui"
+import { AtTabs, AtTabsPane } from 'taro-ui'
+import Taro from '@tarojs/taro'
+import { Env } from '../../env/env'
+import { fmtDate } from '../../fmtDate'
 
 export default class Orgreferral extends Component<PropsWithChildren> {
+  state = {
+    seg: 0,
+  }
 
-  componentWillMount () { }
+  componentWillMount () {
+    Taro.getStorage({
+      key: Env.storageKey,
+      success: res => {
+        let cid = res.data.cid
+        let list
+        Taro.request({
+          url: Env.apiUrl + 'orgs?referrer=' + cid,
+        }).then((res) =>{
+          list = []
+          for (let i of res.data) {
+            list.push(
+              <AtListItem
+              // onClick={}
+              title={i.name}
+              note={i.contact}
+              // extraText={}
+              // arrow='right'
+              className='list-item'
+              />
+            )
+          }
+          this.setState({list1: list})
+        })
+
+        Taro.request({
+          url: Env.apiUrl + 'rewards?&referrer=' + cid,
+          // url: Env.apiUrl + 'refretail/' + 55,
+        }).then((res) =>{
+          console.log(res.data)
+          list = []
+          let record
+          for (let i of res.data) {
+            if (i.type == 0 || i.type == 1) {
+              record = i.ord.orderItems[0]
+            }
+            if (i.type == 2 || i.type == 3) {
+              record = i.retail
+            }
+            list.push(
+              <AtListItem
+              title={record.product.name + ' x ' + record.quantity}
+              note={fmtDate(i.createdAt)}
+              extraText={i.amount / 100}
+              className='list-item'
+              />
+            )
+          }
+          this.setState({list2: list})
+        })
+      }
+    })
+  }
+
+  navTo(page: string) {
+    Taro.navigateTo({ url: '/pages/' + page + '/index' })
+  }
 
   componentDidMount () { }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  handleClick (value) {
+    this.setState({
+      seg: value
+    })
+  }
 
   render () {
+    let tabList = [
+        { title: '我介绍的' },
+        { title: '佣金明细' },
+    ]
     return (
       <View className='orgReferral'>
-        <Text>Hello world!</Text>
+      <AtTabs current={this.state.seg} tabList={tabList} onClick={this.handleClick.bind(this)}>
+        <AtTabsPane current={this.state.seg} index={0}>
+          <AtList>
+          { this.state.list1}
+          </AtList>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.seg} index={1}>
+          <AtList>
+          { this.state.list2}
+          </AtList>
+        </AtTabsPane>
+      </AtTabs>
+
       </View>
     )
   }
