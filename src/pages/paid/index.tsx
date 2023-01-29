bd9f507b417edcb6f6e512f2472a27789c559eb0
Al Zee <z@alz.ee>
2023-01-29 23:59:46 +0800

    page paid done

--- src/pages/paid/index.tsx
+++ src/pages/paid/index.tsx
@@ -1,23 +1,24 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import './index.scss'
+import Taro from '@tarojs/taro'
 
 export default class Paid extends Component<PropsWithChildren> {
 
-  componentWillMount () { }
-
   componentDidMount () { }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  naviBack(){
+    // Taro.redirectTo({ url: '/pages/me/index' })
+    Taro.navigateBack({
+      delta: 1
+    })
+  }
 
   render () {
     return (
-      <View className='paid'>
-        <Text>Hello world!</Text>
+      <View className='paid p-3'>
+        <View className='text'>支付成功</View>
+        <Button className='btn' onClick={this.naviBack}>确认</Button>
       </View>
     )
   }
