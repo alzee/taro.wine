ef9c1f7f520aeb29001206550e11580f17d5375a
Al Zee <z@alz.ee>
2023-01-29 23:20:53 +0800

    setInterval to refresh qr and check if paid

--- src/pages/qr/index.tsx
+++ src/pages/qr/index.tsx
@@ -8,18 +8,31 @@ import { Env } from '../../env/env'
 export default class Qr extends Component<PropsWithChildren> {
   cid: int
   name: string
-  timestamp = new Date().getTime()
+  // timestamp = new Date().getTime()
+  qrIntv: int = 30000
+  qrIntvId: int
+  chkIntv: int = 1000
+  chkIntvId: int
+  state = {
+    timestamp: new Date().getTime()
+  }
 
   componentWillMount () { }
 
-  componentDidMount () { }
-
-  componentWillUnmount () {
-    console.log('bye');
-    Taro.setScreenBrightness({value: -1})
+  newTimestamp = () => {
+    console.log('QR refresh')
+    this.setState({
+      timestamp: new Date().getTime()
+    }, () => {
+      // console.log(this.state.timestamp);
+    })
   }
 
-  componentDidShow () {
+  chkPaid(){
+    console.log('check if paid');
+  }
+
+  componentDidMount () {
     const self = this;
     Taro.getStorage({
       key: Env.storageKey,
@@ -29,22 +42,52 @@ export default class Qr extends Component<PropsWithChildren> {
         this.name = res.data.name
       }
     })
+
+    // Refresh QR every 30 sec
+    this.qrIntvId = setInterval(this.newTimestamp , this.qrIntv)
+    // Check if paid every 1 sec after 5 sec
+    setTimeout(() => {
+      this.chkIntvId = setInterval(this.chkPaid, this.chkIntv)
+      console.log('start checking');
+    }, 5000)
+
     Taro.setScreenBrightness({value: 1})
   }
 
+  componentWillUnmount () {
+    console.log('bye');
+    Taro.setScreenBrightness({value: -1})
+    clearInterval(this.qrIntvId)
+    clearInterval(this.chkIntvId)
+  }
+
   componentDidHide () {
+    // not working
     console.log('bye');
   }
 
+
+  qrclicked = () => {
+    console.log('clicked qr');
+    this.newTimestamp()
+    clearInterval(this.qrIntvId)
+    this.qrIntvId = setInterval(this.newTimestamp , this.qrIntv)
+  }
+
   render () {
-    let text = `{"cid": ${this.cid}, "timestamp": "${this.timestamp}", "name": "${this.name}"}`
     // let text = '{"cid": ${this.cid}, "timestamp": "${this.timestamp}", "name": "${this.name}"}'
     // let text = '{"cid":' + this.cid + ', "timestamp": "' + this.timestamp + '", "name": "' + this.name + '"}'
-    console.log(text)
+    let text
+    if (this.cid !== undefined) {
+      text = `{"cid": ${this.cid}, "timestamp": "${this.state.timestamp}", "name": "${this.name}"}`
+      console.log(text)
+    }
     return (
       <View className='qr'>
+      <View className='text'> 代金券消费 </View>
       { this.cid &&
         <QRCode
+          onClick={this.qrclicked}
           text={text}
           size={200}
           scale={4}
@@ -52,6 +95,7 @@ export default class Qr extends Component<PropsWithChildren> {
           typeNumber={2}
         />
       }
+      <View className='note'> 点击二维码刷新 </View>
       </View>
     )
   }
