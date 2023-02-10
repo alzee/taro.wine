9c386d72e904ba2a3fb0fd225ff2949092b55e30
Al Zee <z@alz.ee>
2023-02-10 12:18:14 +0800

    nth

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -220,12 +220,12 @@ export default class Index extends Component<PropsWithChildren> {
             index: 4
           },
           {
-            image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
+            image: 'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
             value: '销售门店',
             index: 5
           },
           {
-            image: 'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
+            image: 'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
             value: '合作商家',
             index: 6
           },
