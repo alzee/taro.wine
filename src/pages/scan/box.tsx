3e2b5003ff92bf8d85c0b93fb16e4e659ac39c75
Al Zee <z@alz.ee>
2023-02-20 09:33:51 +0800

    page scan box done

--- src/pages/scan/box.tsx
+++ src/pages/scan/box.tsx
@@ -1,11 +1,14 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import './box.scss'
 import { Env } from '../../env/env'
 import Taro from '@tarojs/taro'
 
 export default class Scan extends Component<PropsWithChildren> {
   instance = Taro.getCurrentInstance();
+  state = {
+    data: {}
+  }
 
   componentDidMount () {
     let params = this.instance.router.params
@@ -14,17 +17,21 @@ export default class Scan extends Component<PropsWithChildren> {
       key: Env.storageKey,
       success: res => {
         let data = {
-          oid: res.data.org.id,
+          // oid: res.data.org.id,
+          oid: 27,
           s: params.s,
           e: params.e
         }
         Taro.request({
           method: 'POST',
           data,
-          url: Env.apiUrl + 'scan/bottle',
+          url: Env.apiUrl + 'scan/box',
           success: function (res) { }
         }).then((res) =>{
           console.log(res.data);
+          this.setState({
+            data: res.data
+          })
         })
       }
     })
@@ -37,6 +44,21 @@ export default class Scan extends Component<PropsWithChildren> {
   render () {
     return (
       <View className='scan-box'>
+      <View className='msg'>
+
+      { this.state.data &&
+      <View>
+      {this.state.data.msg}
+      </View>
+      }
+
+      { this.state.data.ord &&
+      <View>
+      {this.state.data.ord.product.name} x {this.state.data.ord.qty}
+      </View>
+      }
+
+      </View>
       <Button className='btn' size='small' onClick={this.done}>确定</Button>
       </View>
     )
