8778a4a3e789bdd27763a9153567e344a593b9f5
Al Zee <z@alz.ee>
2023-02-20 14:05:57 +0800

    nth

--- src/pages/borrow/new.tsx
+++ src/pages/borrow/new.tsx
@@ -1,16 +1,36 @@
 import { Component, PropsWithChildren } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Form, Button, Input, Picker } from '@tarojs/components'
 import './new.scss'
+import { Env } from '../../env/env'
+import Taro from '@tarojs/taro'
 
 export default class Borrow extends Component<PropsWithChildren> {
-  state = {}
+  state = {
+    claims: {}
+  }
 
   componentDidMount () { }
+
+  pickerChange() {
+  }
+
+  formSubmit() {
+  }
 
   render () {
     return (
       <View className='borrow'>
-        <Text>Hello world!</Text>
+      <Form className='form'
+      onSubmit={this.formSubmit.bind(this)}
+      >
+      <Picker mode='selector' range={this.state.claims} onChange={this.pickerChange}>
+      <View className='input'>
+      <Text className='label'>请选择兑奖单</Text>
+      {this.state.selectorChecked}
+      </View>
+      </Picker>
+        <Button className='btn' formType='submit'>提交</Button>
+      </Form>
       </View>
     )
   }
