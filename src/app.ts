445a6a38266850e1f6f092930ac50d1a43bb4743
Al Zee <z@alz.ee>
2023-02-21 23:27:24 +0800

    force logout

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,37 @@ import Taro from '@tarojs/taro'
 import { Env } from './env/env'
 
 class App extends Component<PropsWithChildren> {
+  updateMark = '0'
 
   componentDidMount () {
     // Taro.clearStorage()
     Taro.getStorageInfo({
       success: res => {
         // if not found
-        if (res.keys.indexOf(Env.storageKey) == -1) {
-          // Taro.setStorage({
-          //   key: Env.storageKey,
-          //   data: {uid: 0, role: -1, token: 0}
-          // });
+        // force log out
+        if (res.keys.indexOf(this.updateMark) == -1) {
+          Taro.setStorage({
+            key: this.updateMark,
+            data: 'fuck'
+          });
+          Taro.removeStorage({
+            key: this.updateMark ^ 1,
+            success: res => {
+              console.log('storeage removed: ' + this.updateMark ^ 1);
+            },
+            fail: res => {
+              console.log('storeage remove failed: ' + this.updateMark ^ 1);
+            }
+          })
+          Taro.removeStorage({
+            key: Env.storageKey,
+            success: res => {
+              console.log('storeage removed: ' + Env.storageKey);
+            },
+            fail: res => {
+              console.log('storeage remove failed: ' + Env.storageKey);
+            }
+          })
         }
       },
       fail: res => {
