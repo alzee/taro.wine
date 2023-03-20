9f926bdfb8ea4f33b990ec1a0b343cee29b8e044
Al Zee <z@alz.ee>
2023-03-20 11:45:29 +0800

    page claim/salesman

--- src/pages/claim/salesman.tsx
+++ src/pages/claim/salesman.tsx
@@ -1,22 +1,51 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtList, AtListItem } from "taro-ui"
+import { Env } from '../../env/env'
+import { fmtDate } from '../../fmtDate'
+import Taro from '@tarojs/taro'
+import { Taxon } from '../../Taxon'
 
 export default class Claim extends Component<PropsWithChildren> {
 
-  componentWillMount () { }
+  uid: int
+  oid: int
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  componentDidMount () {
+    Taro.getStorage({
+      key: Env.storageKey
+    })
+    .then(res =>{
+      this.uid = res.data.id
+      this.oid = res.data.org.id
+    })
+    .then(() => {
+      Taro.request({
+        url: Env.apiUrl + 'claim/salesman/' + this.uid
+      }).then((res) =>{
+        let list = []
+        let title = i.prize.name + ' ' + i.product.name
+        let extraText = Taxon.settleStatus[Number(i.status)]
+        for (let i of res.data) {
+          list.push(
+            <AtListItem
+            title={title}
+            note={fmtDate(i.createdAt)}
+            extraText={extraText}
+            />
+          )
+        }
+        this.setState({list})
+    })
+  }
 
   render () {
     return (
       <View className='claim'>
+      <AtList className="list">
+      {this.state.list}
+      </AtList>
       </View>
     )
   }
